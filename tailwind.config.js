335257fe261a2dd368fe0e20a190a7ddb13a51c0
Dhrubaraj Pati <patidhrubaraj@gmail.com>
2025-08-31 19:00:37 +0530

    config font

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,25 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+        cabinet: ['Cabinet Grotesk', 'sans-serif'],
+        general: ['General Sans', 'sans-serif'],
+        boska: ['Boska', 'serif'],
+        switzer: ['Switzer', 'sans-serif'],
+        outfit: ['Outfit', 'sans-serif'],
+        poppins: ['Poppins', 'sans-serif'],
+        supreme: ['Supreme', 'sans-serif'],
+        rowan: ['Rowan', 'serif'],
+        quilon: ['Quilon', 'sans-serif'],
+        satoshi: ['Satoshi', 'sans-serif'],
+        excon: ['Excon', 'sans-serif'],
+        synonym: ['Synonym', 'sans-serif'],
+        amulya: ['Amulya', 'sans-serif'],
+        neco: ['Neco', 'serif'],
+        pally: ['Pally', 'sans-serif'],
+        hind: ['Hind', 'sans-serif']
+      },
       typography: {
         DEFAULT: {
           css: {
