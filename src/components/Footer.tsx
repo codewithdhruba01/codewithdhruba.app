dfdcb69e21f25bb9916d0e1f16dd029cda3e47fe
Dhrubaraj Pati <patidhrubaraj@gmail.com>
2025-11-18 16:44:40 +0530

    update footer color

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ import { Github, Linkedin, Twitter, Coffee } from 'lucide-react';
 
 const Footer = () => {
   return (
-    <footer className="bg-black border-t border-gray-900">
+    <footer className="bg-neutral-950 border-t border-gray-900">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
         <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
           <div className="col-span-1 md:col-span-2">
