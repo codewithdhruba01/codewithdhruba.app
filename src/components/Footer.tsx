72b65aa37cecd338a5968fcf6bf98d316f79e87a
codewithdhruba01 <patidhrubaraj@gmail.com>
2025-12-23 14:48:28 +0530

    update social link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,4 +1,10 @@
-import { Github, Linkedin, Twitter, Coffee } from 'lucide-react';
+import { Coffee } from 'lucide-react';
+import {
+  GithubIcon,
+  LinkedinIcon,
+  XIcon,
+} from '../components/icons/SocialIcons';
+import BlueskyLine from '../components/icons/SocialIcons';
 
 const Footer = () => {
   return (
@@ -19,121 +25,88 @@ const Footer = () => {
               hands-on projects, clear guides, and a commitment to making
               complex concepts accessible to everyone.
             </p>
-            <div className="flex space-x-4">
+
+            {/* Social Icons */}
+            <div className="flex space-x-2">
               <a
                 href="https://github.com/codewithdhruba01"
-                className="p-2 bg-gray-800 hover:bg-gray-200 rounded-lg transition-colors"
+                className="group p-2 rounded-lg transition-all duration-300 ease-in-out hover:bg-gray-800/50"
               >
-                <Github className="w-5 h-5 text-gray-200 hover:text-black" />
+                <GithubIcon size="20" className="group-hover:fill-white transition-colors duration-300" />
               </a>
+
               <a
                 href="https://www.linkedin.com/in/dhrubaraj-pati/"
-                className="p-2 bg-gray-800 hover:bg-[#0162fe] rounded-lg transition-colors"
+                className="group p-2 rounded-lg transition-all duration-300 ease-in-out hover:bg-gray-800/50"
               >
-                <Linkedin className="w-5 h-5 text-gray-400 hover:text-white" />
+                <LinkedinIcon size="20" className="group-hover:fill-white transition-colors duration-300" />
               </a>
+
               <a
                 href="https://x.com/codewithdhruba"
-                className="p-2 bg-gray-800 hover:bg-[#007eb8] rounded-lg transition-colors"
+                className="group p-2 rounded-lg transition-all duration-300 ease-in-out hover:bg-gray-800/50"
               >
-                <Twitter className="w-5 h-5 text-gray-400 hover:text-white" />
+                <XIcon size="20" className="group-hover:fill-white transition-colors duration-300" />
+              </a>
+              <a
+                href="https://bsky.app/profile/codewithdhruba.bsky.social"
+                className="group p-2 rounded-lg transition-all duration-300 ease-in-out hover:bg-gray-800/50"
+              >
+                <BlueskyLine size="21" className="group-hover:fill-white transition-colors duration-300" />
               </a>
             </div>
           </div>
 
           {/* Quick Links */}
           <div>
-            <h3 className="text-lg font-bold font-poppins text-white mb-4">Quick Links</h3>
+            <h3 className="text-lg font-bold font-poppins text-white mb-4">
+              Quick Links
+            </h3>
             <ul className="space-y-1">
               <li>
-                <a
-                  href="/#projects"
-                  className="text-neutral-400 hover:text-green-400 transition-colors font-poppins"
-                >
+                <a href="/#projects" className="text-neutral-400 hover:text-green-400 transition-colors font-poppins">
                   Project
                 </a>
               </li>
               <li>
-                <a
-                  href="/#education"
-                  className="text-neutral-400 hover:text-green-400 transition-colors font-poppins"
-                >
+                <a href="/#education" className="text-neutral-400 hover:text-green-400 transition-colors font-poppins">
                   Education
                 </a>
               </li>
               <li>
-                <a
-                  href="/#skills"
-                  className="text-neutral-400 hover:text-green-400 transition-colors font-poppins"
-                >
+                <a href="/#skills" className="text-neutral-400 hover:text-green-400 transition-colors font-poppins">
                   Skills
                 </a>
               </li>
               <li>
-                <a
-                  href="/#contributions"
-                  className="text-neutral-400 hover:text-green-400 transition-colors font-poppins"
-                >
+                <a href="/#contributions" className="text-neutral-400 hover:text-green-400 transition-colors font-poppins">
                   GitHub Activity
                 </a>
               </li>
             </ul>
           </div>
 
+          {/* Pages */}
           <div>
-            <h3 className="text-lg font-bold font-poppins text-white mb-4">Pages</h3>
+            <h3 className="text-lg font-bold font-poppins text-white mb-4">
+              Pages
+            </h3>
             <ul className="space-y-1">
-              <li>
-                <a
-                  href="/about"
-                  className="text-neutral-400 hover:text-green-400 transition-colors font-poppins"
-                >
-                  About
-                </a>
-              </li>
-              <li>
-                <a
-                  href="/projects"
-                  className="text-neutral-400 hover:text-green-400 transition-colors font-poppins"
-                >
-                  Projects
-                </a>
-              </li>
-              <li>
-                <a
-                  href="/all-posts"
-                  className="text-neutral-400 hover:text-green-400 transition-colors font-poppins"
-                >
-                  Blogs
-                </a>
-              </li>
-              <li>
-                <a
-                  href="/certificates"
-                  className="text-neutral-400 hover:text-green-400 transition-colors font-poppins"
-                >
-                  Achievements
-                </a>
-              </li>
-              <li>
-                <a
-                  href="/contact"
-                  className="text-neutral-400 hover:text-green-400 transition-colors font-poppins"
-                >
-                  Contact Us
-                </a>
-              </li>
+              <li><a href="/about" className="text-neutral-400 hover:text-green-400 transition-colors font-poppins">About</a></li>
+              <li><a href="/projects" className="text-neutral-400 hover:text-green-400 transition-colors font-poppins">Projects</a></li>
+              <li><a href="/all-posts" className="text-neutral-400 hover:text-green-400 transition-colors font-poppins">Blogs</a></li>
+              <li><a href="/certificates" className="text-neutral-400 hover:text-green-400 transition-colors font-poppins">Achievements</a></li>
+              <li><a href="/contact" className="text-neutral-400 hover:text-green-400 transition-colors font-poppins">Contact Us</a></li>
             </ul>
           </div>
         </div>
 
-        <div className="mt-12 pt-8 border-t border-gray-800 flex flex-col  justify-between items-center">
+        <div className="mt-12 pt-8 border-t border-gray-800 flex flex-col justify-between items-center">
           <p className="text-neutral-400 text-sm mb-4 md:mb-0 font-satoshi">
             © 2025. All rights reserved.
           </p>
           <p className="text-neutral-400 text-sm flex items-center font-satoshi">
-            Design & Developed by{' '}
-            <Coffee className="w-4 h-4 mx-1 text-[#fcda03]" />
+            Design & Developed by <Coffee className="w-4 h-4 mx-1 text-[#fcda03]" />
             Dhrubaraj Pati
           </p>
         </div>
--- src/components/icons/SocialIcons.tsx
+++ src/components/icons/SocialIcons.tsx
@@ -4,34 +4,46 @@ type IconProps = {
 };
 
 // 🔹 GitHub
-export const GithubIcon = ({ size, className }: IconProps) => (
-    <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 432 416" className={`${className} fill-[#919191]`}>
-        <path
-            d="M213.5 0q88.5 0 151 62.5T427 213q0 70-41 125.5T281 416q-14 2-14-11v-58q0-27-15-40q44-5 70.5-27t26.5-77q0-34-22-58q11-26-2-57q-18-5-58 22q-26-7-54-7t-53 7q-18-12-32.5-17.5T107 88h-6q-12 31-2 57q-22 24-22 58q0 55 27 77t70 27q-11 10-13 29q-42 18-62-18q-12-20-33-22q-2 0-4.5.5t-5 3.5t8.5 9q14 7 23 31q1 2 2 4.5t6.5 9.5t13 10.5T130 371t30-2v36q0 13-14 11q-64-22-105-77.5T0 213q0-88 62.5-150.5T213.5 0z"
-        />
+export const GithubIcon = ({ size, className = '' }: IconProps) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={size}
+        height={size}
+        viewBox="0 0 432 416"
+        className={`${className} fill-[#919191]`}
+    >
+        <path d="M213.5 0q88.5 0 151 62.5T427 213q0 70-41 125.5T281 416q-14 2-14-11v-58q0-27-15-40q44-5 70.5-27t26.5-77q0-34-22-58q11-26-2-57q-18-5-58 22q-26-7-54-7t-53 7q-18-12-32.5-17.5T107 88h-6q-12 31-2 57q-22 24-22 58q0 55 27 77t70 27q-11 10-13 29q-42 18-62-18q-12-20-33-22q-2 0-4.5.5t-5 3.5t8.5 9q14 7 23 31q1 2 2 4.5t6.5 9.5t13 10.5T130 371t30-2v36q0 13-14 11q-64-22-105-77.5T0 213q0-88 62.5-150.5T213.5 0z" />
     </svg>
 );
 
 // 🔹 LinkedIn
-export const LinkedinIcon = ({ size, className }: IconProps) => (
-    <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 42 42" className={`${className} fill-[#919191]`}>
-        <path
-            d="M6.5 17.5h6v17h-6v-17zm20.141-.58c1.689.01 3.85.84 5.17 2.18c1.369 1.53 1.689 3.259 1.689 5.4v10h-6v-9.29c0-1.7-.689-3.771-2.939-3.84c-1.32.021-2.17.78-2.801 2.06c-.18.42-.14.891-.14 1.36l-.12 9.71h-6v-17h6l.12 2.22a6.19 6.19 0 0 1 1.69-1.829c.96-.691 2.081-.952 3.331-.971zM9.5 9.35c1.54.021 3.07 1.23 3.13 3.07c.04 1.641-1.39 3.07-3.17 3.07h-.04c-1.53 0-3.03-1.25-3.1-3.07c.03-1.62 1.39-3.029 3.18-3.07zM.5 1.5v39h39v-39H.5z"
-        />
+export const LinkedinIcon = ({ size, className = '' }: IconProps) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={size}
+        height={size}
+        viewBox="0 0 42 42"
+        className={`${className} fill-[#919191]`}
+    >
+        <path d="M6.5 17.5h6v17h-6v-17zm20.141-.58c1.689.01 3.85.84 5.17 2.18c1.369 1.53 1.689 3.259 1.689 5.4v10h-6v-9.29c0-1.7-.689-3.771-2.939-3.84c-1.32.021-2.17.78-2.801 2.06c-.18.42-.14.891-.14 1.36l-.12 9.71h-6v-17h6l.12 2.22a6.19 6.19 0 0 1 1.69-1.829c.96-.691 2.081-.952 3.331-.971zM9.5 9.35c1.54.021 3.07 1.23 3.13 3.07c.04 1.641-1.39 3.07-3.17 3.07h-.04c-1.53 0-3.03-1.25-3.1-3.07c.03-1.62 1.39-3.029 3.18-3.07zM.5 1.5v39h39v-39H.5z" />
     </svg>
 );
 
 // 🔹 X (Twitter)
-export const XIcon = ({ size, className }: IconProps) => (
-    <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 24 24" className={`${className} fill-[#919191]`}>
-        <path
-            d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584l-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z"
-        />
+export const XIcon = ({ size, className = '' }: IconProps) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={size}
+        height={size}
+        viewBox="0 0 24 24"
+        className={`${className} fill-[#919191]`}
+    >
+        <path d="M18.901 1.153h3.68l-8.04 9.19L24 22.846h-7.406l-5.8-7.584l-6.638 7.584H.474l8.6-9.83L0 1.154h7.594l5.243 6.932ZM17.61 20.644h2.039L6.486 3.24H4.298Z" />
     </svg>
 );
 
-// 🔹 Instagram
-export const InstagramIcon = ({ size, className }: IconProps) => (
+// 🔹 Instagram (stroke-based)
+export const InstagramIcon = ({ size, className = '' }: IconProps) => (
     <svg
         xmlns="http://www.w3.org/2000/svg"
         width={size}
@@ -49,25 +61,29 @@ export const InstagramIcon = ({ size, className }: IconProps) => (
     </svg>
 );
 
-
 // 🔹 Daily.dev
-export const DailydotdevIcon = ({ size, className }: IconProps) => (
-    <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 24 24" className={`${className} fill-[#919191]`}>
-        <path
-            d="M18.29 5.706a1.405 1.405 0 0 0-1.987 0L4.716 17.296l1.324-2.65l-2.65-2.649l3.312-3.311l2.65 2.65l1.986-1.988l-3.642-3.642a1.405 1.405 0 0 0-1.987 0L.411 11.004a1.404 1.404 0 0 0 0 1.987l4.305 4.304l.993.993a1.405 1.405 0 0 0 1.987 0L19.285 6.7l-.993-.994Zm-.332 3.647l2.65 2.65l-4.306 4.305a1.404 1.404 0 1 0 1.986 1.986l5.299-5.298a1.404 1.404 0 0 0 0-1.987l-4.305-4.304z"
-        />
+export const DailydotdevIcon = ({ size, className = '' }: IconProps) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={size}
+        height={size}
+        viewBox="0 0 24 24"
+        className={`${className} fill-[#919191]`}
+    >
+        <path d="M18.29 5.706a1.405 1.405 0 0 0-1.987 0L4.716 17.296l1.324-2.65l-2.65-2.649l3.312-3.311l2.65 2.65l1.986-1.988l-3.642-3.642a1.405 1.405 0 0 0-1.987 0L.411 11.004a1.404 1.404 0 0 0 0 1.987l4.305 4.304l.993.993a1.405 1.405 0 0 0 1.987 0L19.285 6.7l-.993-.994Zm-.332 3.647l2.65 2.65l-4.306 4.305a1.404 1.404 0 1 0 1.986 1.986l5.299-5.298a1.404 1.404 0 0 0 0-1.987l-4.305-4.304z" />
     </svg>
 );
 
-// 🔹 Codepen
-export const CodepenIcon = ({ size, className }: IconProps) => (
-    <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 24 24" className={`${className} stroke-[#919191]`}>
-        <g
-            fill="none"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-        >
+// 🔹 Codepen (stroke-based)
+export const CodepenIcon = ({ size, className = '' }: IconProps) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={size}
+        height={size}
+        viewBox="0 0 24 24"
+        className={`${className} stroke-[#919191]`}
+    >
+        <g fill="none" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2">
             <path d="m12 2l10 6.5v7L12 22L2 15.5v-7L12 2zm0 20v-6.5" />
             <path d="m22 8.5l-10 7l-10-7" />
             <path d="m2 15.5l10-7l10 7M12 2v6.5" />
@@ -76,10 +92,28 @@ export const CodepenIcon = ({ size, className }: IconProps) => (
 );
 
 // 🔹 Forem / News
-export const NewsIcon = ({ size, className }: IconProps) => (
-    <svg xmlns="http://www.w3.org/2000/svg" width={size} height={size} viewBox="0 0 26 26" className={`${className} fill-[#919191]`}>
-        <path
-            d="M16.906 1.969H1A1 1 0 0 0 0 3v15.969c0 2.365 1.319 3.818 2.563 4.437C3.806 24.025 5 24 5 24h16.094c.175 0 1.176-.026 2.313-.594c1.186-.593 2.427-1.959 2.53-4.125V8.031s-.004-.702-.375-1.438c-.337-.667-1.147-1.4-2.281-1.53H18V3.187A1 1 0 0 0 17 2zM2 4h14v14.969c0 1.28.406 2.275.969 3.031H5s-.454.002-1-.156C2.681 21.249 2 20.68 2 18.969V4zm2 2c-.551 0-1 .449-1 1v2c0 .551.449 1 1 1h10c.551 0 1-.449 1-1V7c0-.551-.449-1-1-1H4z"
-        />
+export const NewsIcon = ({ size, className = '' }: IconProps) => (
+    <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={size}
+        height={size}
+        viewBox="0 0 26 26"
+        className={`${className} fill-[#919191]`}
+    >
+        <path d="M16.906 1.969H1A1 1 0 0 0 0 3v15.969c0 2.365 1.319 3.818 2.563 4.437C3.806 24.025 5 24 5 24h16.094c.175 0 1.176-.026 2.313-.594c1.186-.593 2.427-1.959 2.53-4.125V8.031s-.004-.702-.375-1.438c-.337-.667-1.147-1.4-2.281-1.53H18V3.187A1 1 0 0 0 17 2zM2 4h14v14.969c0 1.28.406 2.275.969 3.031H5s-.454.002-1-.156C2.681 21.249 2 20.68 2 18.969V4zm2 2c-.551 0-1 .449-1 1v2c0 .551.449 1 1 1h10c.551 0 1-.449 1-1V7c0-.551-.449-1-1-1H4z" />
     </svg>
 );
+
+
+const BlueskyLine = ({ size, className = '' }: IconProps) => (
+    <svg xmlns="http://www.w3.org/2000/svg"
+        width={size}
+        height={size}
+        viewBox="0 0 24 24"
+        className={`${className} fill-[#919191]`}
+    >
+        <path d="M4.907 3.44c.709.12 1.533.501 2.576 1.247C9.28 5.97 10.769 7.739 12 9.564c1.231-1.825 2.72-3.593 4.517-4.877c1.043-.746 1.867-1.127 2.576-1.248c.767-.13 1.29.065 1.615.2C21.717 4.06 22 5.225 22 6.215c0 .201-.086 1.302-.185 2.36c-.052.546-.11 1.108-.166 1.577c-.053.428-.115.88-.19 1.133a4.331 4.331 0 0 1-1.951 2.517c.937.707 1.277 1.856.928 2.984c-.597 1.924-2.737 4.397-4.835 4.618c-1.773.186-2.935-1.325-3.601-2.76c-.666 1.435-1.828 2.946-3.6 2.76c-2.099-.22-4.24-2.694-4.836-4.618c-.35-1.128-.01-2.277.928-2.984a4.331 4.331 0 0 1-1.95-2.517c-.076-.253-.138-.705-.19-1.133a60.58 60.58 0 0 1-.167-1.577C2.085 7.517 2 6.416 2 6.215c0-.99.283-2.155 1.292-2.576c.326-.135.847-.33 1.615-.2M4.05 6.916c.064.818.18 2.108.288 2.992c.032.268.055.545.122.807c.397 1.34 1.929 2.082 3.686 1.887a1 1 0 0 1 .257 1.983c-.756.112-3.315.357-2.928 1.607c.357 1.151 1.848 3.087 3.134 3.222c.928.097 1.5-1.394 1.766-2.045c.275-.674.488-1.38.668-1.982a1 1 0 0 1 1.916 0c.18.602.393 1.308.668 1.982c.266.651.838 2.142 1.766 2.045c1.286-.135 2.777-2.07 3.134-3.222c.387-1.25-2.172-1.495-2.928-1.607a1 1 0 0 1 .257-1.983c1.757.195 3.289-.547 3.686-1.887c.069-.266.089-.536.122-.807a80.35 80.35 0 0 0 .288-2.992c.037-.468.247-1.637-.522-1.506c-.286.049-.823.241-1.749.903c-1.943 1.389-3.705 3.513-4.789 5.64a1 1 0 0 1-1.782 0c-1.084-2.127-2.846-4.251-4.789-5.64c-.926-.662-1.463-.854-1.749-.903c-.784-.134-.559 1.034-.522 1.506" />
+    </svg>
+);
+
+export default BlueskyLine;
